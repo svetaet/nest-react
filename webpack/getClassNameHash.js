f6a095fd72bb6817b6cff8090218c202eec53c51
Radysh Arseniy <senyrad@gmail.com>
2020-06-19 20:15:51 +0300

    Stable css class names for long term caching

--- webpack/getClassNameHash.js
+++ webpack/getClassNameHash.js
@@ -1,42 +1,11 @@
-const q = [],
-	cache = {}
+const loaderUtils = require('loader-utils')
+const path = require('path')
 
-function randomNaming(length, limit) {
-	let result = ''
-	const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_'
-	/*All valid chars*/
-	const fchars = 'abcdefghjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-	/*All valid first chars*/
-
-	do {
-		if (q.length >= 52 * Math.pow(64, limit - 1) && limit >= length) {
-			return 'OutOfPossibility'
-		} else if (q.length >= 52 * Math.pow(64, limit - 1) && limit < length) {
-			++limit
-		}
-		result = ''
-		result += fchars[Math.floor(Math.random() * fchars.length)]
-		for (let i = limit - 1; i > 0; --i) {
-			result += chars[Math.floor(Math.random() * chars.length)]
-		}
-	} while (q.includes(result))
-	q.push(result) /*push for avoiding collision in next time of funtion call*/
-	return result
-}
-
-module.exports = (loaderContext, localIdentName, localName) => {
-	let randName = randomNaming(3, 2)
-	if (localName.match(/^i-/i)) {
-		randName = `i-${randName}`
-	} else if (localName.match(/^i_/i)) {
-		randName = `i_`
-	} else {
-		randName = `${randName}`
-	}
-	if (typeof cache[localName] == 'undefined') {
-		cache[localName] = randName
-		return cache[localName]
-	} else {
-		return cache[localName]
-	}
-}
+module.exports = (context, localIdentName, localName) =>
+	'_' +
+	loaderUtils.getHashDigest(
+		path.posix.relative(context.rootContext, context.resourcePath) + localName,
+		'md5',
+		'base64',
+		4,
+	)
